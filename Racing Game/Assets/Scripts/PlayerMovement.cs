6a1634a4e050dc41ec5a414bae26c333cf0ce419
Wu Haoxiang <WuHaoxiang@WudeMacBook-Pro.local>
2015-07-07 00:00:16 +0800

    do more on playermovement and create the spriteanimation script!

--- Racing Game/Assets/Scripts/PlayerMovement.cs
+++ Racing Game/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
 	private float moveDirection = 0.0f;
 	private float turnDirection = 0.0f;
 
+	public float currentSpeed = 0.0f;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,37 +21,44 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
+		// Drag and AngularDrag variables
+		currentSpeed = Mathf.Abs (transform.InverseTransformDirection (GetComponent<Rigidbody> ().velocity).z);
+		
+		float maxAngularDrag = 2.5f;
+		float currentAngularDrag = 1.0f;
+		float aDragLerpTime = currentSpeed * 0.1f;
+		
+		float maxDrag = 1.0f;
+		float currentDrag = 2.5f;
+		float dragLerpTime = currentSpeed * 0.1f;
+		
+		float myAngularDrag = Mathf.Lerp (currentAngularDrag, maxAngularDrag, aDragLerpTime);
+		float myDrag = Mathf.Lerp (currentDrag, maxDrag, dragLerpTime);
+
+		//Acceleration movement
 		if (Input.GetAxis ("Vertical") > 0.0f)
 		{
 			moveDirection = Input.GetAxis ("Vertical") * speed;
 			GetComponent<Rigidbody> ().AddRelativeForce (0, 0, moveDirection);
-
-			turnDirection = Input.GetAxis ("Horizontal") * turnSpeed;
-			GetComponent<Rigidbody> ().AddRelativeTorque(0, turnDirection, 0);
+			if(currentSpeed > 0.05f)
+			{
+				turnDirection = Input.GetAxis ("Horizontal") * turnSpeed;
+				GetComponent<Rigidbody> ().AddRelativeTorque(0, turnDirection, 0);
+			}
 		}
-
+		//Reverse movement
 		if(Input.GetAxis("Vertical") < 0.0f)
 		{
 			moveDirection = Input.GetAxis ("Vertical") * reverseSpeed;
 			GetComponent<Rigidbody> ().AddRelativeForce (0, 0, moveDirection);
-
-			turnDirection = Input.GetAxis ("Horizontal") * turnSpeed;
-			GetComponent<Rigidbody> ().AddRelativeTorque(0, -turnDirection, 0);
+			if(currentSpeed > 0.05f)
+			{
+				turnDirection = Input.GetAxis ("Horizontal") * turnSpeed;
+				GetComponent<Rigidbody> ().AddRelativeTorque(0, -turnDirection, 0);
+			}
 		}
 
-		float currentSpeed = Mathf.Abs (transform.InverseTransformDirection (GetComponent<Rigidbody> ().velocity).z);
-
-		float maxAngularDrag = 2.5f;
-		float currentAngularDrag = 1.0f;
-		float aDragLerpTime = currentSpeed * 0.1f;
-
-		float maxDrag = 1.0f;
-		float currentDrag = 2.5f;
-		float dragLerpTime = currentSpeed * 0.1f;
-
-		float myAngularDrag = Mathf.Lerp (currentAngularDrag, maxAngularDrag, aDragLerpTime);
-		float myDrag = Mathf.Lerp (currentDrag, maxDrag, dragLerpTime);
-
+		//calculate drag and angulardrag
 		GetComponent<Rigidbody> ().angularDrag = myAngularDrag;
 		GetComponent<Rigidbody> ().drag = myDrag;
 	}
