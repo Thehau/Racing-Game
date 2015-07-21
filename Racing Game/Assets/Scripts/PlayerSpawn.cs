7aed44933374b7efc2365b347168daa96cb03a07
Wu Haoxiang <WuHaoxiang@WudeMacBook-Pro.local>
2015-07-21 21:54:31 +0800

    create deadzone add mud and powerup pfbs , when the playercar hit the powerup it will get a log!

---
+++ Racing Game/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	public Transform playerSpawn;
+	public Vector3 currentTrackPosition;
+
+	public bool activeRespawnTimer = false;
+	public float respawnTimer = 1.0f;
+	public float resetRespawnTimer = 1.0f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (playerSpawn != null) 
+		{
+			transform.position = playerSpawn.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (activeRespawnTimer) 
+		{
+			respawnTimer -= Time.deltaTime;
+		}
+
+		if (respawnTimer <= 0.0f) 
+		{
+			transform.position = currentTrackPosition;
+			respawnTimer = resetRespawnTimer;
+			activeRespawnTimer = false;
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "CheckPoint")
+		{
+			currentTrackPosition = transform.position;
+		}
+
+		if (other.tag == "DeadZone")
+		{
+			activeRespawnTimer = true;
+		}
+	}
+}
---
+++ Racing Game/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour 
+{
+	public enum PowerType
+	{
+		Projectile = 0,
+		Trap = 1,
+		Boost = 2
+	}
+
+	public PowerType powerUpType = PowerType.Projectile;
+	private GameObject playerGameObject;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerGameObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Debug.Log("We have a poerup !");
+			ApplyPowerUp();
+		}
+	}
+
+	void ApplyPowerUp()
+	{
+		switch (powerUpType) 
+		{
+		case PowerType.Projectile :
+			Debug.Log("We have a projectile!");
+			break;
+		case PowerType.Trap :
+			Debug.Log("We have a trap!");
+			break;
+		case PowerType.Boost :
+			Debug.Log("We have a boost!");
+			break;
+		}
+	}
+}
